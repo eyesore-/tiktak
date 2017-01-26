31bb6d63474c7dd111390b609041d2c4fd54987d
Joel <joelbmolina@gmail.com>
2017-01-26 12:29:16 -0700

    added win combinations and validating move

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const prompt = require('prompt')
 
+// TIC TAC TOE BOARD
 //   1  |  2  |  3
 // -----------------
 //   4  |  5  |  6
@@ -7,6 +8,11 @@ const prompt = require('prompt')
 //   7  |  8  |  9
 
 let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let winCombos = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [3, 4, 6]
+]
 
 function printBoard () {
   console.log(
@@ -18,5 +24,16 @@ function printBoard () {
       ${board[6]}  |  ${board[7]}  |  ${board[8]}  \n`
   )
 }
+
+function markMove (pos, player) {
+  board[pos - 1] = player
+}
+
+function validateMove (pos) {
+  if (Number.isInteger(pos) && Number.isInteger(board[pos] && (pos >= 0 && pos < 9))) return true
+  return false
+}
+
+function checkWin
 
 printBoard()
